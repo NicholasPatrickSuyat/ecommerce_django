8b367daa91bca02e8d13307078530b9aa91e2122
Nicholas Patrick Suyat <nicholaspatricksuyat@gmail.com>
2024-05-15 22:09:45 -0600

    make migrations basic

--- user/models.py
+++ user/models.py
@@ -1,18 +1,27 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth.models import AbstractUser
+# from django.db import models
 
-class Profile(AbstractUser):
-    name = models.CharField(max_length=100,blank=True)
-    picture = models.ImageField(upload_to='profile_pictures', blank=True, null=True)
-    # Initial name as a default picture
+# class UserProfile(models.Model):
+#     user = models.OneToOneField('CustomUser', on_delete=models.CASCADE)
+#     bio = models.TextField(blank=True)
+#     # Add other profile fields as needed
 
-    def __str__(self):
-        return self.username
+#     def __str__(self):
+#         return self.user.username + "'s Profile"
 
-class Order(models.Model):
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    product_name = models.CharField(max_length=100)
+# class Order(models.Model):
+#     user = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='orders')
+#     product_name = models.CharField(max_length=100)
+#     order_date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Order {self.id} by {self.user.username}"
-    
+#     def __str__(self):
+#         return f"Order {self.id} by {self.user.username}"
+
+# class CustomUser(AbstractUser):
+#     # Add custom fields here
+#     name = models.CharField(max_length=150, blank=True)
+#     email = models.EmailField(unique=True)
+#     # Add other fields as needed
+
+#     def __str__(self):
+#         return self.username
