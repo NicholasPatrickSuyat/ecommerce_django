658e1c8e7db7e8589ce67919c2526bfd054885ae
Nicholas Patrick Suyat <nicholaspatricksuyat@gmail.com>
2024-05-21 20:07:13 -0600

    home templates

--- contact/models.py
+++ contact/models.py
@@ -3,9 +3,8 @@ from django.db import models
 class ContactMessage(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
+    phone_number = models.CharField(max_length=15)  # New phone number field
     message = models.TextField()
 
-    
     def __str__(self):
         return self.name
-
