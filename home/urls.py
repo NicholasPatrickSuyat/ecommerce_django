ee318a036a6cdcac0b7655eeeec5e42308bb0489
Nicholas Patrick Suyat <nicholaspatricksuyat@gmail.com>
2024-05-22 21:41:21 -0600

    templates

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, learn_more_1
 
 app_name = 'home'
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('learn_1/', learn_more_1, name='learn-more-1'),
+    path('learn_2/', learn_more_1, name='learn-more-2'),
+    path('learn_3/', learn_more_1, name='learn-more-3'),
+    path('learn_4/', learn_more_1, name='learn-more-4'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,15 @@ from .models import CompanyVideo # Import the TrainingVideo model
 def home_view(request):
     videos = CompanyVideo.objects.all()  # Fetch all videos from the database
     return render(request, 'home/home.html', {'company_videos': videos})
+
+def learn_more_1(request):
+    return render(request, 'home/learn1.html')
+
+def learn_more_2(request):
+    return render(request, 'home/learn2.html')
+
+def learn_more_3(request):
+    return render(request, 'home/learn3.html')
+
+def learn_more_4(request):
+    return render(request, 'home/learn4.html')
