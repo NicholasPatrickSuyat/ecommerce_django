e252de4fe561064a6285793cc5e80bb9aacb3d99
Nicholas Patrick Suyat <nicholaspatricksuyat@gmail.com>
2024-06-16 15:12:38 -0600

    more templates

--- myapp/settings.py
+++ myapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-92!x3l11ohdr@&24qqg@j%@2425&+3u64-!_jcu9uh4+%1#b(&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
