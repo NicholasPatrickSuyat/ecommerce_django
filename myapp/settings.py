3f68c2e25d1253f3f0f32ce50a0a42effb762d1e
Nicholas Patrick Suyat <nicholaspatricksuyat@gmail.com>
2024-06-30 16:42:59 -0600

    fixing webhook

--- myapp/settings.py
+++ myapp/settings.py
@@ -141,7 +141,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# Logging configuration
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -154,7 +153,7 @@ LOGGING = {
     },
     'formatters': {
         'simple': {
-            'format': '{asctime} {levelname} {message}',
+            'format': '{asctime} {levelname} {name} {message}',
             'style': '{',
         },
     },
@@ -166,16 +165,20 @@ LOGGING = {
         'django': {
             'handlers': ['console'],
             'level': 'DEBUG',
-            'propagate': False,
+            'propagate': True,
         },
         'cart': {
             'handlers': ['console'],
             'level': 'DEBUG',
-            'propagate': False,
+            'propagate': True,
+        },
+        'paypal_utils': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
         },
     },
 }
-
 
 
 
